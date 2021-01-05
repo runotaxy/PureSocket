6cae69d922fe0d8c2a09c865da57f9fc391406e8
runotaxy <onurcelik.dev@gmail.com>
2021-01-05 21:37:11 +0300

    main class methods added.. + example page created..

---
+++ PureSocket.js
@@ -0,0 +1,24 @@
+class PureSocket {
+	constructor(){
+		console.log("[ PureSocket.js ] : init..");
+	}
+
+	connect(socket_address){
+		if( typeof PS_Socket == "undefined" ){
+			window.PS_Socket = new WebSocket(socket_address);
+			PS_Socket.onopen = ()=>{ this.connection_message(); }
+		}else{
+			console.log("[ PureSocket.js ] : Socket is already declared");
+		}
+	}
+
+	connection_message(){
+		console.log("[ PureSocket.js ] : Connected to the socket..");
+	}
+
+	listen(signal, callback){
+		PS_Socket.onmessage = function(data){
+			callback(data);
+		}
+	}
+}
